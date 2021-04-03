6c5bb6fabdfc5a3572feee97d7b01d1133f09448
Rayton Lin <39320843+raytonlin1@users.noreply.github.com>
2021-04-03 00:40:32 -0400

    Update deque.c

--- deque.c
+++ deque.c
@@ -1,18 +1,3 @@
-///////////////////////////////////////////////////////////////////////////// 
-// INTEGRITY STATEMENT (v3)
-//
-// By signing your name and ID below you are stating that you have agreed
-// to the online academic integrity statement:
-// https://student.cs.uwaterloo.ca/~cs136/current/assignments/integrity.shtml
-/////////////////////////////////////////////////////////////////////////////
-// I received help from and/or collaborated with: 
- 
-// None
-//  
-// Name: Rayton Lin 
-// login ID: r56lin 
-///////////////////////////////////////////////////////////////////////////// 
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
